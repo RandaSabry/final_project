001fc7e787ae114c0667ed1de913ded0f7f5181d
Randa Sabri <127007934+RandaSabry@users.noreply.github.com>
2023-03-18 22:59:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let closebtn = document.querySelector('.closeNav')
+let openbtn = document.querySelector('.openNav')
+let mySidenav= document.querySelector('#mySidenav')
+
+closebtn.addEventListener('click',()=>{
+    closebtn.style.width="0"
+})
+
+openbtn.addEventListener('click',()=>{
+    openbtn.style.width="0"
+})
